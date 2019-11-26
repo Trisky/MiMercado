868e427e55f702eec9260b2e4b27358b3f559150
Trisky <Trisky@users.noreply.github.com>
2019-11-26 22:59:57 +0100

    Request error handling in React

--- resources/js/components/Products/Catalog.js
+++ resources/js/components/Products/Catalog.js
@@ -1,19 +1,39 @@
 import React, {Component} from 'react'
 import axios from 'axios';
-class Catalog extends Component{
-    componentDidMount(){
+
+class Catalog extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {success: false};
         axios.get('/api/products')
             .then(response => response.data)
-            .then(products =>this.setState({products}));
+            .then(products => {
+                    this.setState({products: products, success: true})
+                }
+            )
+            .catch
+            (response => {
+                    this.setState({products: [], success: false})
+                    console.error('failed to retieve products');
+                    alert('error retrieving products')
+                }
+            );
     }
 
-    render(){
-        console.log(this.state);
-        return (
-            <div className='catalog'>
-                Here should we see the list with our products
-            </div>
-        )
+    render() {
+        if (this.state.success) {
+            let products = this.state.products;
+            console.log(products);
+            return (
+                <div className='catalog'>
+                    Here should we see the list with our products
+                </div>
+            )
+        } else {
+            return <div>loading</div>;
+        }
     }
 }
 export default Catalog;
