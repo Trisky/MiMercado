c3368bee47a5fdc238a5e7426215b3f9a08ce88c
Trisky <Trisky@users.noreply.github.com>
2019-12-05 18:14:39 +0100

    Added 'catalog not found'

--- resources/js/Router.js
+++ resources/js/Router.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {BrowserRouter, Link, Route, Switch} from 'react-router-dom';
 import Login from './components/Login';
 import CatalogContainer from './components/Products/CatalogContainer';
+import Notfound from './components/Products/Notfound';
 
 const Main = props => (
     <Switch>
         <Route exact path='/' component={Login}/>
         <Route exact path='/app/Catalog/:username' component={CatalogContainer}/>
+        <Route exact path='/app/notfound' component={Notfound}/>
     </Switch>
 );
 export default Main;
---
+++ resources/js/components/Products/Notfound.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+
+class Notfound extends Component {
+    render() {
+        return <div className="alert alert-warning col-md-6 col-4  mx-auto text-center ">
+            <strong>The catalog required does not exist</strong>
+        </div>
+    }
+}
+export default Notfound;
