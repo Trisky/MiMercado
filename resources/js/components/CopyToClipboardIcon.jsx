8b600084f0f801f4f36259ee2a962a899968fb10
Trisky <Trisky@users.noreply.github.com>
2019-12-13 18:47:14 +0100

    Added nice icon for copy to clipboard

--- resources/js/components/CopyToClipboardIcon.jsx
+++ resources/js/components/CopyToClipboardIcon.jsx
@@ -1,14 +1,16 @@
 import React, {Component} from "react";
 import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { IoIosCopy } from "react-icons/io";
 
 export default class CopyToClipboardIcon extends Component {
-
     render() {
         let text = this.props.text;
         return (
             <CopyToClipboard text={text}
-                             onCopy={() => this.setState({copied: true})}>
-                <button>copy</button>
+                             title='copy to clipboard'
+                             onCopy={() => this.setState({copied: true})}
+            >
+                <IoIosCopy/>
             </CopyToClipboard>)
     }
 }
