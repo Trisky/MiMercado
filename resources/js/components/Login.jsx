eb2827e5d66ba15b23e8022cd6a90084a8ed15b3
Trisky <Trisky@users.noreply.github.com>
2019-12-13 20:09:35 +0100

    Empty modal for when you have no products

--- resources/js/components/Login.jsx
+++ resources/js/components/Login.jsx
@@ -1,19 +1,20 @@
 import React, {Component} from 'react'
+import { IoMdSad } from "react-icons/io";
 
-class Login extends Component {
+class GenericModal extends Component {
     render() {
         return <div className={' py-4 row justify-content-center'}>
             <div className={'col-md-6'}>
                 <div className={'card'}>
                     <div className={'card-header'}>
-                        Login to Mercado Libre to list your products
+                        {this.header}
                     </div>
                     <div className={'card-body'}>
                         <div className={'row'}>
-                            <div className={'col-md-6'}>
-                                <a className={'btn btn-primary'} href={'/melilogin'}> Log in to Mercado Libre
-                                </a>
-                            </div>
+                            {this.body1}
+                        </div>
+                        <div className={'row'}>
+                            {this.body2}
                         </div>
                     </div>
                 </div>
@@ -21,4 +22,27 @@ class Login extends Component {
         </div>
     }
 }
-export default Login
+
+export class Login extends GenericModal {
+    header = 'Login to Mercado Libre to list your products';
+    body1 = <div className={'col-md-6 '}>
+        <a className={'btn btn-primary'} href={'www.mercadolibre.com.ar/publicar'}>
+            Log in to Mercado Libre
+        </a>
+    </div>;
+    body2 = '';
+
+}
+export class EmptyCatalog extends GenericModal {
+    header = <div>Your catalog is empty  <IoMdSad/> </div>;
+    body1 = '';
+    body2 = <div className={'col-md-6 '}>
+        <a className={'btn btn-primary'} href={'/melilogin'}>
+            Publish something!
+        </a>
+    </div>;
+
+}
+
+
+
--- resources/js/components/Products/Catalog.jsx
+++ resources/js/components/Products/Catalog.jsx
@@ -1,14 +1,20 @@
 import React, {Component} from 'react'
 import Product from './Product'
+import {EmptyCatalog} from "../Login";
 
 class Catalog extends Component {
     render() {
         let products = this.props.products.map(product => {
             return <Product key={product.id} data={product}/>
         });
-        return <div className="grid">
-            <div className={'row justify-content-center'}> {products}</div>
-        </div>
+        if(products.length >0){
+            return <div className="grid">
+                <div className={'row justify-content-center'}> {products}</div>
+            </div>
+        }else{
+            return <EmptyCatalog/>
+        }
+
     }
 }
 export default Catalog;
