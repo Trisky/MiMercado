ff1666167543db059a4db00c3aa824c9d2dada93
Trisky <Trisky@users.noreply.github.com>
2019-12-14 23:46:19 +0100

    Better component abstractions

--- resources/js/components/Modal.jsx
+++ resources/js/components/Modal.jsx
@@ -3,19 +3,14 @@ import { IoMdSad } from "react-icons/io";
 
 class Modal extends Component {
     render() {
-        return <div className={' py-4 row justify-content-center'}>
+        return <div className={'py-4 row justify-content-center'}>
             <div className={'col-md-6'}>
                 <div className={'card'}>
                     <div className={'card-header'}>
                         {this.header}
                     </div>
                     <div className={'card-body'}>
-                        <div className={'row'}>
-                            {this.body1}
-                        </div>
-                        <div className={'row'}>
-                            {this.body2}
-                        </div>
+                        {this.body}
                     </div>
                 </div>
             </div>
@@ -23,25 +18,33 @@ class Modal extends Component {
     }
 }
 
+const Row = props => {
+    return <div className={'row'}>
+        {props.body}
+    </div>;
+};
+const ButtonShort = props => {
+    return <div className={'col-md-6 '}>
+        <a className={'btn btn-primary'} href={props.href}>
+            {props.body}
+        </a>
+    </div>
+};
+
 export class LoginModal extends Modal {
     header = 'Login to Mercado Libre to list your products';
-    body1 = <div className={'col-md-6 '}>
-        <a className={'btn btn-primary'} href={'/melilogin'}>
-            Log in to Mercado Libre
-        </a>
-    </div>;
-    body2 = '';
-
+    body =
+        <Row body={
+            <ButtonShort body='Log in to Mercado Libre' href='/melilogin'/>
+        }/>
 }
-export class EmptyCatalog extends Modal {
-    header = <div>Your catalog is empty  <IoMdSad/> </div>;
-    body1 = '';
-    body2 = <div className={'col-md-6 '}>
-        <a className={'btn btn-primary'} href={'www.mercadolibre.com.ar/publicar'}>
-            Publish something!
-        </a>
-    </div>;
 
+export class EmptyCatalog extends Modal {
+    header = <div>Your catalog is empty <IoMdSad/></div>;
+    body =
+        <Row body={
+            <ButtonShort body='Publish something!' href='www.mercadolibre.com.ar/publicar'/>
+        }/>
 }
 
 
